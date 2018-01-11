7d3f9c98f824dad4c5082eae45cb94f77569df01
Scot <scot.pfleghaar@gmail.com>
2018-01-11 16:11:06 -0600

    Finished Crud Commands

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const methodOverride = require('method-override');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
+
+
+///////////DATABASE////////////
 
 //Map Global Promises - Removes Warning
 mongoose.Promise = global.Promise;
@@ -18,6 +22,9 @@ mongoose.connect('mongodb://test:test@ds249787.mlab.com:49787/idea-dev',{
 require("./models/Idea");
 const Idea = mongoose.model("ideas");
 
+
+///////////MIDDLEWARE////////////
+
 //Handlebard Middleware
 app.engine("handlebars",exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
@@ -25,6 +32,12 @@ app.set('view engine', 'handlebars');
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
+
+///////////ROUTES////////////
 
 //Index Route
 app.get("/", (req, res)=>{
@@ -39,9 +52,32 @@ app.get("/about", (req,res)=>{
     res.render("about");
 });
 
+// Idea Index Page
+app.get('/ideas',(req,res)=>{
+    Idea.find({}).sort({date:'desc'}).then(ideas =>{
+        res.render('ideas/index',{
+            ideas:ideas
+        });
+    })
+    
+})
+
 // Add Idea Form
 app.get("/ideas/add", (req,res)=>{
     res.render("ideas/add");
+});
+
+
+// Edit Idea Form
+app.get("/ideas/edit/:id", (req,res)=>{
+    Idea.findOne({
+        _id: req.params.id
+    }).then(idea =>{
+        res.render("ideas/edit",{
+            idea: idea
+        });
+    });
+    
 });
 
 // Process Form
@@ -61,9 +97,40 @@ app.post('/ideas',(req,res)=>{
             detials: req.body.detials
         });
     } else {
-        res.send("Passed");
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details,
+            // user: req.user.id
+        }
+        new Idea(newUser).save().then(idea => {
+            res.redirect('/ideas');
+        });
     }
 })
+
+// Edit form Process
+app.put('/ideas/:id',(req, res)=>{
+    Idea.findOne({
+        _id: req.params.id
+    }).then(idea =>{
+        // new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+        idea.save().then(idea => {
+            res.redirect('/ideas');
+        });
+    });
+});
+
+//DELETE IDEA
+app.delete('/ideas/:id',(req,res)=>{
+    Idea.remove({
+        _id: req.params.id
+    }).then(()=>{
+        res.redirect('/ideas');
+    });
+})
+
 
 const port = 5000;
 
