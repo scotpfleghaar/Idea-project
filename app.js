0b2f5bd79008657177e8224c3a5b5659da0f26ba
Scot <scot.pfleghaar@gmail.com>
2018-01-11 13:27:20 -0600

    Created The Database Model

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//Map Global Promises - Removes Warning
+mongoose.Promise = global.Promise;
 //Connect to DB
+mongoose.connect('mongodb://test:test@ds249787.mlab.com:49787/idea-dev',{
     useMongoClient: true
 }).then(()=>{
     console.log("MongoDB Connected");
 }).catch(err => console.log(err));
+
+//Load idea model
+require("./models/Idea");
+const Idea = mongoose.model("ideas");
+
 //Handlebard Middleware
 app.engine("handlebars",exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
