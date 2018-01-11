1e6599e5791a393257acff3d77eb84413ec532ae
Scot <scot.pfleghaar@gmail.com>
2018-01-11 14:14:59 -0600

    Complete The Add Idea Form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -21,6 +22,10 @@ const Idea = mongoose.model("ideas");
 app.engine("handlebars",exphbs({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 //Index Route
 app.get("/", (req, res)=>{
    const title = "Welcome";
@@ -33,6 +38,33 @@ app.get("/", (req, res)=>{
 app.get("/about", (req,res)=>{
     res.render("about");
 });
+
+// Add Idea Form
+app.get("/ideas/add", (req,res)=>{
+    res.render("ideas/add");
+});
+
+// Process Form
+app.post('/ideas',(req,res)=>{
+    let errors = [];
+    if(!req.body.title){
+        errors.push({text:"Please add a Title"});
+    }
+    if(!req.body.details){
+        console.log("Called");
+        errors.push({text:"Please add Details"});
+    }
+    if(errors.length > 0){
+        res.render('ideas/add',{
+            errors: errors,
+            title: req.body.title,
+            detials: req.body.detials
+        });
+    } else {
+        res.send("Passed");
+    }
+})
+
 const port = 5000;
 
 app.listen(port, () => {
