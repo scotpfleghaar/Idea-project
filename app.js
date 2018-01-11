5b038b2137e17f9889e813cbc4edc6e3ff543eb7
Scot <scot.pfleghaar@gmail.com>
2018-01-11 13:18:20 -0600

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 
 const app = express();
 
+//Connect to DB
+    useMongoClient: true
+}).then(()=>{
+    console.log("MongoDB Connected");
+}).catch(err => console.log(err));
+//Handlebard Middleware
+app.engine("handlebars",exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+//Index Route
+app.get("/", (req, res)=>{
+   const title = "Welcome";
+    res.render("index", {
+        title: title
+    });
+});
+
+//About Route
+app.get("/about", (req,res)=>{
+    res.render("about");
+});
 const port = 5000;
 
 app.listen(port, () => {
